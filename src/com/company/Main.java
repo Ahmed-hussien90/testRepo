ca6776c2bfbb0a627276ac1d50e03d5dffd02e4e
Ahmed-hussien90 <Ahmed.hs9090@gmail.com>
2021-12-15 00:40:37 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Ahmed");
+
+        System.out.println("second commit");
+
+
 	// write your code here
     }
 }
